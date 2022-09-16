fc252654911601fd0e7ef430d41ed080610b826e
Aluno <aluno>
2022-09-15 22:06:26 -0300

    Aula 15/09

--- app/src/main/java/br/com/up/listadepresentes/MainActivity.java
+++ app/src/main/java/br/com/up/listadepresentes/MainActivity.java
@@ -1,12 +1,15 @@
 package br.com.up.listadepresentes;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
@@ -20,17 +23,23 @@ public class MainActivity extends AppCompatActivity {
 
     private FloatingActionButton fabAddGift;
     private RecyclerView recyclerViewGifts;
+    private TextView textViewGiftsNotFound;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        textViewGiftsNotFound = findViewById(R.id.text_gifts_not_found);
         recyclerViewGifts = findViewById(R.id.recycler_gifts);
         fabAddGift = findViewById(R.id.fab_add_gift);
 
-        recyclerViewGifts.setAdapter(new GiftAdapter());
-
+        recyclerViewGifts.setLayoutManager(
+               // new GridLayoutManager(this,10)
+                new LinearLayoutManager(this,
+                        RecyclerView.VERTICAL,
+                        false)
+        );
 
         fabAddGift.setOnClickListener(
                 new View.OnClickListener() {
@@ -52,6 +61,17 @@ public class MainActivity extends AppCompatActivity {
         super.onResume();
         ArrayList<Gift> gifts =
                 GiftRepository.getInstance().getAll();
+
+        if(gifts.size() > 0){
+            textViewGiftsNotFound.setVisibility(View.INVISIBLE);
+        }else{
+            textViewGiftsNotFound.setVisibility(View.VISIBLE);
+        }
+
+
+        recyclerViewGifts.setAdapter(
+                new GiftAdapter(gifts)
+        );
 
     }
 }
